bd11888e2113d6a87486797406dbbec270120d69
monicabocelli <monica.bocelli93@gmail.com>
2017-02-20 18:06:18 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function draw() {
   textSize(height/12);
   text("touch here", width/2, height/2);
   
-   if(mouseX > width/2 - width/7 && mouseX < width/2 + width/7 && mouseY > height/2 - width/7 && mouseY <  height/2 + width/7){    
+   if(touchX > width/2 - width/7 && touchX < width/2 + width/7 && touchY > height/2 - width/7 && touchY <  height/2 + width/7){    
    image(myImage2,0,0,windowWidth,windowHeight);  
    }
 }
