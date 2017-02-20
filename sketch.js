0aff417752fa70bf5aea7d483939e0bfa56e6ffb
monicabocelli <monica.bocelli93@gmail.com>
2017-02-20 18:45:51 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,13 +14,18 @@ function draw() {
   stroke(250);
   rectMode(CENTER);
   rect(width/2, height/2, width/7, width/7);
-  noStroke();
-  fill(250);
-  textSize(height/12);
-  text("touch here", width/2, height/2);
   
    if(touchX > width/2 - width/7 && touchX < width/2 + width/7 && touchY > height/2 - width/7 && touchY <  height/2 + width/7){    
    image(myImage2,0,0,windowWidth,windowHeight);  
+       
+      noFill();
+      stroke(250);
+      rectMode(CENTER);
+      rect(width/3, height/3, width/7, width/7);
+       if(touchX > width/3 - width/7 && touchX < width/3 + width/7 && touchY > height/3 - width/7 && touchY <  height/3 + width/7){ 
+           background(150);
+           ellipse(width/2, height/2, width/5,width/5);
+       }   
    }
 }
 
